e4bfb7ef9368d81686f6a95d0e944035019380dd
lightmon <lightmon5210@outlook.com>
2026-06-26 16:38:26 +0800

    add changewallpaper shotcut

--- config.def.h
+++ config.def.h
@@ -82,6 +82,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 static const char *screenshotcmd[] = { "sh", "-c", "mkdir -p ~/Screenshots && scrot -s ~/Screenshots/%Y-%m-%d_%H-%M-%S.png", NULL }; /* use scrot to do screenshots */
+static const char *changewallpapercmd[] = { "sh", "-c", "feh --bg-fill --randomize ~/Wallpapers", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -128,6 +129,7 @@ static const Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
   { MODKEY|ShiftMask,             XK_s,      spawn,          {.v = screenshotcmd } }, /* screenshot keybind */
+  { MODKEY|ShiftMask,             XK_n,      spawn,          {.v = changewallpapercmd } },
 
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
