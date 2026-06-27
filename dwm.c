f9ba5033f2df30e812073b29ecafd2fbde9bd9b4
lightmon <lightmon5210@outlook.com>
2026-06-27 14:53:05 +0800

    try to fix: bring setscheme back

--- dwm.c
+++ dwm.c
@@ -810,6 +810,7 @@ drawbar(Monitor *m)
   w = m->ww - tw - x - gap;
 	if (w > bh) {
 		if (m->sel) {
+      drw_setscheme(drw, scheme[SchemeNorm]);
 			drw_setscheme(drw, scheme[m == selmon ? SchemeSel : SchemeNorm]);
 			drw_text(drw, x, 0, w, bh, lrpad / 2, m->sel->name, 0);
 			if (m->sel->isfloating)
