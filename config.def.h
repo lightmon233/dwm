6178f599f065290e3ebdad981fc626ef5f2d70d9
lightmon <lightmon5210@outlook.com>
2026-06-28 22:08:34 +0800

    update changewallpapercmd

--- config.def.h
+++ config.def.h
@@ -90,7 +90,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 static const char *screenshotcmd[] = { "sh", "-c", "mkdir -p ~/Screenshots && scrot -s ~/Screenshots/%Y-%m-%d_%H-%M-%S.png", NULL }; /* use scrot to do screenshots */
-static const char *changewallpapercmd[] = { "sh", "-c", "feh --bg-fill --randomize ~/Wallpapers", NULL };
+static const char *changewallpapercmd[] = { "sh", "-c", "wal -i ~/Wallpapers & xrdb -merge ~/.cache/wal/colors.Xresources & xdotool key alt+F5", NULL };
 static const char scratchpadname[] = "scratchpad";
 static const char *scratchpadcmd[] = { "alacritty", "--title", scratchpadname, "--class", "scratchpad", NULL };
 
